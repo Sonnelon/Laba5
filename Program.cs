c165d6719df0a3b235433b40a4a42e5f092a8f6a
John Doe <johndoe@example.com>
2022-10-26 19:37:50 +0300

    Comment

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class programm
+{
+
+    static void Main()
+    {
+        Console.WriteLine("Введите сумму вклада");
+
+        double value = double.Parse(Console.ReadLine());
+
+        if (value < 100)
+        {
+            value = value + value * 0.05;
+        }
+        else if (value < 200)
+        {
+            value = value + value * 0.07;
+        }
+        else
+        {
+            value = value + value * 0.1;
+        }
+
+        Console.WriteLine(value);
+
+    }
+}
